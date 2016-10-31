07edbca170a990471e8a7cf89d14aa6c0a63cfa3
walkermannum1 <2469377884@qq.com>
2016-10-31 19:13:54 +0800

    Add some code.

--- app/src/main/java/com/example/user/kgend/WorkdayActivity.java
+++ app/src/main/java/com/example/user/kgend/WorkdayActivity.java
@@ -1,10 +1,19 @@
 package com.example.user.kgend;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.RelativeLayout;
 
 import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
 import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.services.core.LatLonPoint;
 import com.amap.api.services.route.BusRouteResult;
 import com.amap.api.services.route.DriveRouteResult;
 import com.amap.api.services.route.RideRouteResult;
@@ -15,9 +24,41 @@ import com.amap.api.services.route.WalkRouteResult;
  * Created by user on 2016/10/31.
  */
 public class WorkdayActivity extends Activity implements LocationSource, AMapLocationListener, RouteSearch.OnRouteSearchListener {
+    private AMap mAMap;
+    private MapView mMapView;
+    private Context mContext;
+    private RouteSearch mRouteSearch;
+    private RideRouteResult mRideResult;
+    private RelativeLayout mBottomLayout;
+    private LatLonPoint mStartPoint = new LatLonPoint(118, 34);
+    private LatLonPoint mEndPoint = new LatLonPoint(118, 33);
+    private OnLocationChangedListener mListener;
+    private AMapLocationClient mLocationClient;
+    private AMapLocationClientOption mClientOption;
+    private final int ROUTE_TYPE_RIDE = 4;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.basemap_activity);
+        mMapView = (MapView) findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        init();
+        setFormantoMaker();
+        searchRouteResult(ROUTE_TYPE_RIDE, RouteSearch.RidingDefault);
+    }
+
+    private void searchRouteResult(int route_type_ride, int ridingDefault) {
+    }
+
+    private void setFormantoMaker() {
+    }
+
+    private void init() {
+    }
+
     @Override
     public void onLocationChanged(AMapLocation aMapLocation) {
-
     }
 
     @Override
