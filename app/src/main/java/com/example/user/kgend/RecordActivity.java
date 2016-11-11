085245af7471f19fb8ad296e9769bf4e7b4ed52e
walkermannum1 <2469377884@qq.com>
2016-11-11 17:22:19 +0800

    Add record layout.

--- app/src/main/java/com/example/user/kgend/RecordActivity.java
+++ app/src/main/java/com/example/user/kgend/RecordActivity.java
@@ -1,13 +1,30 @@
 package com.example.user.kgend;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 2016/11/10.
  */
 public class RecordActivity extends Activity implements AdapterView.OnItemClickListener {
+    private RecordAdapter mRecordAdapter;
+    private ListView mAllRecordListView;
+    private DbAdapter mDbAdapter;
+    private List<PathRecord> mAllRecord = new ArrayList<PathRecord>();
+    public static final String RECORD_ID = "Record_id";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
